ab54ccfaa520d4d068082572c0949bcb33b9ec03
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-04-07 10:26:19 +0530

    Add files via upload

--- array/ARRAY.C
+++ array/ARRAY.C
@@ -2,6 +2,8 @@
 #include<conio.h>
 int linear_search(int [], int, int);
 int binary_search(int [], int, int);
+void traverse(int [], int);
+void unsort_insert(int [], int, int, int);
 void main(){
    int arr[100], n, item, i, res, choice;
    clrscr();
@@ -11,7 +13,7 @@ void main(){
    for(i=0; i<n; i++){
      scanf("%d", &arr[i]);
    }
-   printf("Enter the function you want to perform: \n1.Linear Search ");
+   printf("Enter the function you want to perform: \n1. Linear Search\n2. Binary Search\n3. Traverse ");
    scanf("%d", &choice);
    switch(choice){
    case 1:
@@ -22,8 +24,17 @@ void main(){
        printf("Element not in the array");
      else
        printf("Element found at: %d position", res + 1);
-   getch();
+
+   case 2:
+     printf("Enter the element you want to search for: ");
+     scanf("%d", &item);
+     res = binary_search(arr, n, item);
+     if(res == -1)
+       printf("Element not in the array");
+     else
+       printf("Element found at: %d position", res + 1);
   }
+  getch();
 }
 int linear_search(int a[], int n, int item){
    int i;
@@ -34,3 +45,25 @@ int linear_search(int a[], int n, int item){
    }
    return -1;
 }
+int binary_search(int a[], int n, int item){
+   int i, l, r, mid;
+   l = 0;
+   r = n - 1;
+   while (l <= r){
+     mid = (l + r) / 2;
+     if(a[mid] < item)
+	l = mid + 1;
+     else if(a[mid] > item)
+	r = mid - 1;
+     else
+	return mid;
+   }
+   return -1;
+}
+void traverse(int a[], int n){
+  int i;
+  for(i = 0; i < n; i++){
+     printf("%d", a[i];
+  }
+}
+void unsort_insert(int a[], int n, int loc, int item){
