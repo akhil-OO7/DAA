b81d55865a021aa85ef3f503e37ba9457924f1fb
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-04-11 11:53:05 +0530

    Update linkedlist.c

--- Linked List/linkedlist.c
+++ Linked List/linkedlist.c
@@ -7,10 +7,12 @@ typedef struct nodeType{
 node *head = NULL;
 void insertAtBeg(node **, int);
 void traverse(node *);
+void insertAtEnd(node **, int);
+
 int main() {
     int choice, item, i = 0;
     while(1){
-    printf("Enter your choice: \n1. Insert at Beginning\n2. Traverse\n3. Exit ");
+    printf("Enter your choice: \n1. Insert at Beginning\n2. Insert at Ending\n3. Traverse\n4. Exit ");
     scanf("%d", &choice);
     switch(choice){
         case 1:
@@ -20,14 +22,20 @@ int main() {
             break;
         
         case 2:
-            traverse(head);
+            printf("Enter the data: ");
+            scanf("%d", &item);
+            insertAtEnd(&head, item);
             break;
         
+        case 3:
+            traverse(head);
+            printf("NULL\n");
+            break;
+            
         default:
             exit(0);
         }
-    }
-    printf("Program End");
+    };
     return 0;
 }
 void insertAtBeg(node **head, int item){
@@ -39,6 +47,20 @@ void insertAtBeg(node **head, int item){
     else
         ptr -> next = *head;
     *head = ptr;
+}
+void insertAtEnd(node **head, int item){
+    node *ptr;
+    ptr = (node *)malloc(sizeof(node));
+    ptr -> info = item;
+    node *tmp = *head;
+    if (*head == NULL){
+        *head = ptr;
+        return;
+    }
+    while(tmp -> next != NULL){
+        tmp = tmp -> next;
+    }
+    tmp -> next = ptr;
 }
 void traverse(node * head){
     while(head != NULL){
