0ac82735008f13ebb3b0c4147f7b9ba77e50378c
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-04-21 09:51:06 +0530

    Update addpoly.c

--- Linked List/addpoly.c
+++ Linked List/addpoly.c
@@ -4,14 +4,60 @@
 typedef struct nodeType{
     int power, coeff;
     struct nodeType *next;
-}
+}node;
 
 node *poly1 = NULL;
 node *poly2 = NULL;
+node *poly3 = NULL;
 
-void main(){
-    while(1){
-        createPoly(&poly1, c, p);
+void createPoly(node **, int, int);
+void addPoly(node *, node *, node **);
+
+void main() {
+    int choice, c, p;
+    while(1) {
+        printf("Enter your choice:\n1. Create 1st Polynomial\n2. Create 2nd Polynomial\n3. Traverse\n4. Add\t");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                printf("Enter coefficient and power of x of Polynomial 1: ");
+                scanf("%d%d", &c, &p);
+                createPoly(&poly1, c, p);
+                break;
+            
+            case 2:
+                printf("Enter coefficient and power of x of Polynomial 2: ");
+                scanf("%d%d", &c, &p);
+                createPoly(&poly2, c, p);
+                break;
+                
+            case 3:
+                int ch;
+                printf("Enter the Polynomial you want to display (1, 2, 3) :");
+                scanf("%d", &ch);
+                switch(ch){
+                    case 1:
+                        traverse(poly1);
+                        break;
+                    
+                    case 2:
+                        traverse(poly2);
+                        break;
+                        
+                    case 3:
+                        traverse(poly3);
+                        break;
+                        
+                    default:
+                        printf("Wrong Choice Entered");
+            
+                }
+                addPoly(poly1, poly2, &poly3);
+                
+            default:
+                exit(0);
+        }
+        
     }
 }
 
@@ -49,5 +95,16 @@ void addPoly(node *poly1, node *poly2, node **poly3){
         }
         poly1 = poly1 -> next;
         poly2 = poly2 -> next;
+    }
+}
+
+void traverse(node *tmp) {
+    if(tmp == NULL){
+        printf("NULL");
+        return;
+    }
+    while(tmp){
+        printf("%dx^%d + ", tmp -> coeff, tmp -> power);
+        tmp = tmp -> next;
     }
 }
