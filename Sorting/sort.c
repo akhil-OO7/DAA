ea92a4c2ae790ff053882e66d890767125d8f4ae
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-05-05 10:22:14 +0530

    Update sort.c

--- Sorting/sort.c
+++ Sorting/sort.c
@@ -56,7 +56,7 @@ void selectionSort(int arr[], int n) {
     for(int i = 0; i < n - 1; i++){
         minIdx = i;
         for(int j = i + 1; j < n; j++) {
-            if(arr[j] < arr[j + 1]){
+            if(arr[j] < arr[minIdx]){
                 minIdx = j;
             }
         }
